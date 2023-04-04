3cfeb264237dc8da7f3bb085beb3deae82940896
viitry <florianvitry@hotmail.fr>
2023-04-04 18:52:03 +0200

    connect

--- lib/connexion.dart
+++ lib/connexion.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ConnexionPage extends StatefulWidget {
   @override
@@ -20,6 +23,119 @@ class _ConnexionPageState extends State<ConnexionPage> {
   }
 
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color.fromRGBO(241, 249, 255, 1),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/logo.jpeg',
+                height: 150.0,
+              ),
+              SizedBox(height: 15),
+              // Bonjour
+              Text('Bonjour !',
+                  style: GoogleFonts.bebasNeue(
+                    fontSize: 50,
+                  )),
+              SizedBox(height: 15),
+
+              Text(
+                'Heureux de vous retrouver !',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 35),
+
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          border: Border.all(
+                            width: 0.5,
+                            color: Color.fromRGBO(55, 89, 119, 1),
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // champ de texte pour l'email
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20.0),
+                              child: TextField(
+                                controller: _emailController,
+                                decoration: InputDecoration(
+                                  isDense: true,
+                                  hintText: 'Email',
+                                ),
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+
+                            // champ de texte pour le mot de passe
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20.0),
+                              child: TextField(
+                                controller: _passwordController,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  isDense: true,
+                                  hintText: 'Mot de passe',
+                                ),
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ),
+
+                            SizedBox(
+                              height: 40.0,
+                            ),
+
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 90.0),
+                              child: TextButton(
+                                onPressed: _connecter,
+                                style: TextButton.styleFrom(
+                                  backgroundColor:
+                                      Color.fromRGBO(46, 88, 123, 100),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 30.0),
+                                ),
+                                child: Text(
+                                  'Sign in',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /*@override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
@@ -111,7 +227,7 @@ class _ConnexionPageState extends State<ConnexionPage> {
         ],
       ),
     );
-  }
+  }*/
 }
 
 class MyClipper extends CustomClipper<Path> {
