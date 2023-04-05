6062337758c99e11ecc4ce9fe43b35f29c53b983
viitry <florianvitry@hotmail.fr>
2023-04-05 20:03:37 +0200

    accueil

--- lib/accueil.dart
+++ lib/accueil.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AccueilPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(241, 249, 255, 1),
       appBar: AppBar(
         title: Text('Accueil'),
         actions: <Widget>[
@@ -15,67 +17,70 @@ class AccueilPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Expanded(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/logo.jpeg',
+                      height: 125.0,
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Container(
+                  height: 38,
+                  child: Expanded(
                     child: TextField(
                       decoration: InputDecoration(
-                        hintText: 'Rechercher',
+                        hintText: 'Rechercher un plat, une ville ...',
+                        hintStyle: GoogleFonts.imprima(fontSize: 13),
                         border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+                          borderRadius: BorderRadius.circular(25),
                         ),
                         filled: true,
                         fillColor: Colors.white,
                       ),
                     ),
                   ),
-                  SizedBox(width: 16.0),
-                  IconButton(
-                    icon: Icon(Icons.sort),
-                    onPressed: () {
-                      // Afficher les options de tri
-                    },
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/ngannou.PNG',
-                      height: 200.0,
-                    ),
-                    SizedBox(height: 16.0),
-                    Text(
-                      'Mon titre',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 8.0),
-                    Text(
-                      'Ma description',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ],
                 ),
               ),
-            ),
-          ],
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: 16.0),
+                      Text(
+                        'Mon titre',
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      Text(
+                        'Ma description',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
